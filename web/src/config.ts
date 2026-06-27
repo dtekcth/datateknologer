976634fda7e4bb2f697f57a41537a2f10999dc66
yhdtk <yahya.fakierah@dtek.se>
2026-06-27 19:34:23 +0200

    Update config.ts

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,54 +1,33 @@
-import chairmanProfile from "./assets/profiles/chairman.jpg"
-import financialManagerProfile from "./assets/profiles/financialManager.jpg"
-import eventsProfile from "./assets/profiles/events.png"
-import logisticsProfile from "./assets/profiles/logistics.jpg"
-import marketingProfile from "./assets/profiles/marketing.jpg"
-import customerRelationsProfile from "./assets/profiles/customerRelations.jpg"
-import communicationsProfile from "./assets/profiles/communications.jpg"
+import chairmanProfile from "./assets/profiles/chairman.jpeg"
+import financialManagerProfile from "./assets/profiles/tresurer.jpeg"
+import viceChairmanProfile from "./assets/profiles/vicechairman.jpeg"
+import prProfile from "./assets/profiles/prmanager.jpeg"
 
 export const config = {
   profiles: [
     {
       pictureUrl: chairmanProfile,
-      name: "Martin Martinsson",
+      name: "Benjamin Amiri",
       title: "Chairman",
-      email: "martin.martinsson@dtek.se",
+      email: "benjamin.amiri@dtek.se",
     },
     {
       pictureUrl: financialManagerProfile,
-      name: "Leo Wivhagen",
+      name: "Dylan Babahajian",
       title: "Treasurer",
-      email: "leo.wivhagen@dtek.se",
+      email: "dylan.babahajian@dtek.se",
     },
     {
-      pictureUrl: marketingProfile,
-      name: "Axel Marklén",
-      title: "Member",
-      email: "axel.marklen@dtek.se",
+      pictureUrl: viceChairmanProfile,
+      name: "Yahya Fakierah",
+      title: "Vice Chairman",
+      email: "yahya.fakeirah@dtek.se",
     },
     {
-      pictureUrl: eventsProfile,
-      name: "Melvin Berg",
-      title: "Member",
-      email: "melvin.berg@dtek.se",
-    },
-    {
-      pictureUrl: customerRelationsProfile,
-      name: "Lukas Söderberg",
-      title: "Member",
-      email: "lukas.soderberg@dtek.se",
-    },
-    {
-      pictureUrl: communicationsProfile,
-      name: "Daniel Husberg",
-      title: "Member",
-      email: "daniel.husberg@dtek.se",
-    },
-    {
-      pictureUrl: logisticsProfile,
-      name: "Jesper Wärn",
-      title: "Member",
-      email: "jesper.warn@dtek.se",
+      pictureUrl: prProfile,
+      name: "Patrik Ae",
+      title: "PR Manager",
+      email: "patrik.ae@dtek.se",
     },
   ],
 };
