ef34d5169d2a0745b6db61dd5a6771dd0369dc45
Tejune <0skarianftw@gmail.com>
2026-07-13 23:38:00 +0200

    This website is a mess :(

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -169,7 +169,7 @@ export default defineComponent({
     content: "";
     position: absolute;
     inset: 0;
-    background-image: url("../assets/theone.jpg");
+    background-image: url("../assets/dag_group_photo.jpg");
     background-size: cover;
     mix-blend-mode: hard-light;
     filter: blur(calc(2px * var(--deg-intensity)));
@@ -219,13 +219,13 @@ section {
 }
 
 .parallax.landing::after {
-  background: url("../assets/theone.jpg");
+  background: url("../assets/dag_group_photo.jpg");
   background-size: cover;
   transform: translateZ(-1px) scale(1.5);
 }
 
 .landing:not(.parallax) {
-  background: url("../assets/theone.jpg");
+  background: url("../assets/dag_group_photo.jpg");
   background-size: cover;
   background-position: center top;
 }
